23c90ce99f98bf52552121419102aaecbfb0b6a3
cerencelik25 <45871252+cerencelik25@users.noreply.github.com>
2024-05-24 19:24:02 -0400

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,46 +1,38 @@
 import torch
 from torch import nn
 
-
 def create_linear_regression_model(input_size, output_size):
-    
     model = nn.Linear(input_size, output_size)
     return model
 
-
 def train_iteration(X, y, model, loss_fn, optimizer):
-    # Compute prediction and loss
     pred = model(X)
     loss = loss_fn(pred, y)
 
-    # Backpropagation
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
     return loss
 
-
 def fit_regression_model(X, y):
-    
-    learning_rate = 0.01 # Pick a better learning rate
-    num_epochs = 100 # Pick a better number of epochs
-    input_features = X.shape[1] # extract the number of features from the input `shape` of X
-    output_features = y.shape[1] if len(y.shape) > 1 else 1 # extract the number of features from the output `shape` of y
+    learning_rate = 1e-3 
+    num_epochs = 1000  
+    input_features = X.shape[1]
+    output_features = y.shape[1] if len(y.shape) > 1 else 1
     model = create_linear_regression_model(input_features, output_features)
-    
-    loss_fn = nn.MSELoss() # Use mean squared error loss, like in class
 
+    loss_fn = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
     previos_loss = float("inf")
 
-    for epoch in range(1, num_epochs):
+    for epoch in range(1, num_epochs + 1):
         loss = train_iteration(X, y, model, loss_fn, optimizer)
-        if abs(previous_loss - loss.item()): # Change this condition to stop the training when the loss is not changing much.
+        if abs(previos_loss - loss.item()) < 1e-6:
             break
-        if eposch % 1000 == 0: #Print loss every 1000 epochs
+        if epoch % 100 == 0:
             print(f'Epoch {epoch}, Loss: {loss.item()}')
         previos_loss = loss.item()
-        # This is a good place to print the loss every 1000 epochs.
+
     return model, loss
 
